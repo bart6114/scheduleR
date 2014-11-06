64ec34b7e74eec64dd6712b137b5bfe33421aa88
Bart6114 <bartsmeets86@gmail.com>
2014-11-06 08:31:25 +0100

    minor adj...

--- R/run_rscript.R
+++ R/run_rscript.R
@@ -1,4 +1,3 @@
-
 args <- commandArgs(trailingOnly = TRUE)
 
 script_to_run <- args[1]
@@ -8,10 +7,18 @@ commandArgs <- function() args
 
 setwd(
   dirname(script_to_run)
-  )
+)
+
+print(44)
 
 tryCatch({
-source(
-  basename(script_to_run), echo=TRUE
+  source(
+    basename(script_to_run), echo=TRUE
   )},
-error = function(e) quit(save = "no", status = 1))
+  error = function(e){
+    cat("\n\n\n")
+    cat("========================\n")
+    cat("An error was detected.\n");
+    print(e)
+    quit(save = "no", status = 1)
+  })
