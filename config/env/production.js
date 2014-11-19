d1137b1f79cd528fd7933b74076cbe4b18030c92
Bart6114 <bartsmeets86@gmail.com>
2014-11-19 22:57:24 +0100

    added grunt copy task for bootstrap fonts

--- config/env/production.js
+++ config/env/production.js
@@ -5,18 +5,18 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				//'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				//'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				//'public/lib/angular/angular.min.js',
+				//'public/lib/angular-resource/angular-resource.js',
+				//'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				//'public/lib/angular-ui-utils/ui-utils.min.js',
+				//'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
 			]
 		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
+		css: 'public/dist/css/application.min.css',
+		js: 'public/dist/js/application.min.js'
 	}
 };
