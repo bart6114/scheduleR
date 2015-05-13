fd06c20b40e44c30b9b4c54e37b92582eaee6ab7
Bart <Bart6114@users.noreply.github.com>
2015-05-13 14:16:07 +0200

    updated to comply with crontab standards

--- public/modules/reports/services/schedule.client.service.js
+++ public/modules/reports/services/schedule.client.service.js
@@ -6,8 +6,8 @@ angular.module('reports').factory('ScheduleService',
         var scheduleOptions = {
             minutes: ['*'].concat(_.range(0,60)).map(String),
             hours: ['*'].concat(_.range(0,24)).map(String),
-            days: ['*'].concat(_.range(0,32)).map(String),
-            months: ['*'].concat(_.range(0,12)).map(String),
+            days: ['*'].concat(_.range(1,32)).map(String),
+            months: ['*'].concat(_.range(1,13)).map(String),
             weekdays: ['*'].concat(_.range(0,7)).map(String)
         };
 
